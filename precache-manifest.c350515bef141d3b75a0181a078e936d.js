b2e00e3d0ce10ad1c88ffc168581488f2dc9dab8
Deep Diwakar <deepdiwakar9@gmail.com>
2020-04-22 16:07:56 +0530

    Updates

---
+++ precache-manifest.c350515bef141d3b75a0181a078e936d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152f489b2e31eb6659cf600b1adcf677",
+    "url": "/QuizApp_React/index.html"
+  },
+  {
+    "revision": "51ff7cf94182f7db6f86",
+    "url": "/QuizApp_React/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "1538cf265ebce00dd566",
+    "url": "/QuizApp_React/static/js/2.2175e3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizApp_React/static/js/2.2175e3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ff7cf94182f7db6f86",
+    "url": "/QuizApp_React/static/js/main.a38670ea.chunk.js"
+  },
+  {
+    "revision": "2488aadd3370aaad59fd",
+    "url": "/QuizApp_React/static/js/runtime-main.13b1f5ef.js"
+  }
+]);
