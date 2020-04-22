96f412051cd1517175d4bbde5d76126433a7d377
vipul1609 <35247913+vipul1609@users.noreply.github.com>
2020-04-22 16:52:42 +0530

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Result = ({score, playAgain}) => (
   <div className="score-board">
-    <div className="score">You scored {score} / 5 correct answers! Fuck YOu!</div>
+    <div className="score">You scored {score} / 5 correct answers!</div>
     <button className="playBtn" onClick={playAgain}>
       Play again!
     </button>
